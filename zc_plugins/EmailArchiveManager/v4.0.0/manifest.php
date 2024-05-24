98b4b3a9574c7e354bd79c8d60d9b36b63d6d7da
Chris Brown <code@drbyte.dev>
2024-05-23 21:12:43 -0400

    Update manifest.php

--- zc_plugins/EmailArchiveManager/v4.0.0/manifest.php
+++ zc_plugins/EmailArchiveManager/v4.0.0/manifest.php
@@ -6,8 +6,8 @@ return [
     'pluginDescription' => 'View and manage archived emails/logs.',
     'pluginAuthor' => 'Destination Imagination and others',
     'pluginId' => 101, // ID from Zen Cart forum
-    'zcVersions' => ['v200'],
-    'changelog' => '', // online URL (eg github release tag page, or changelog file there) or local filename only, ie: changelog.txt (in same dir as this manifest file)
-    'github_repo' => '', // url
+    'zcVersions' => ['v200', 'v201', 'v210'],
+    'changelog' => 'https://github.com/zencart/email-archive-manager/releases', // online URL (eg github release tag page, or changelog file there) or local filename only, ie: changelog.txt (in same dir as this manifest file)
+    'github_repo' => 'https://github.com/zencart/email-archive-manager/', // url
     'pluginGroups' => [],
 ];
